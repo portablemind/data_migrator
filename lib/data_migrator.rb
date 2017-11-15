50d0bcd09826fb01097b126b0f003d6667753291
rholmes <russellfholmes@gmail.com>
2017-11-15 08:45:34 -0500

    bumped version

--- lib/data_migrator.rb
+++ lib/data_migrator.rb
@@ -61,21 +61,21 @@ module RussellEdge
           migrations(path).each do |migration|
             source = File.read(migration[:filename])
             source = "# This migration comes from #{scope} (originally #{migration[:version]})\n#{source}"
-            
+
             if duplicate = destination_migrations.detect { |m| m[:name] == migration[:name] }
               if options[:refresh] && duplicate[:scope] == scope.to_s
                 Dir.glob(File.join(destination,"*_#{migration[:name].underscore}.#{scope.to_s}.rb")).each { |f| puts "Removing old migration #{migration[:name]}"; File.delete(f) }
               elsif options[:on_skip] && duplicate[:scope] != scope.to_s
-                options[:on_skip].call(scope, migration) 
+                options[:on_skip].call(scope, migration)
               end
               next unless options[:refresh]
             end
-            
+
             migration[:version] = next_migration_number(last ? last[:version] + 1 : 0).to_i unless options[:preserve_timestamp]
             new_path = File.join(destination, "#{migration[:version]}_#{migration[:name].underscore}.#{scope}.rb")
             old_path, migration[:filename] = migration[:filename], new_path
             last = migration
-            
+
             File.open(migration[:filename], "w") { |f| f.write source }
             copied << migration
             options[:on_copy].call(scope, migration, old_path) if options[:on_copy]
@@ -229,7 +229,7 @@ module RussellEdge
             klass.send(action.to_s)
           end
         end
-      rescue Exception => ex
+      rescue StandardError => ex
         raise ex
       end
       time_str = "(%.4fs)" % time.real
--- lib/data_migrator/version.rb
+++ lib/data_migrator/version.rb
@@ -2,7 +2,7 @@ module DataMigrator
   module VERSION #:nodoc:
     MAJOR = 2
     MINOR = 0
-    TINY  = 1
+    TINY  = 2
 
     STRING = [MAJOR, MINOR, TINY].compact.join('.')
   end
