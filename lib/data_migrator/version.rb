135f8111057d11b51849064608f287729aa0240e
Russell Holmes <rholmes@truenorthtechnology.com>
2018-05-03 10:15:59 -0400

    updated version to support Rails 5

--- lib/data_migrator/version.rb
+++ lib/data_migrator/version.rb
@@ -1,8 +1,8 @@
 module DataMigrator
   module VERSION #:nodoc:
-    MAJOR = 2
+    MAJOR = 3
     MINOR = 0
-    TINY  = 2
+    TINY  = 0
 
     STRING = [MAJOR, MINOR, TINY].compact.join('.')
   end
