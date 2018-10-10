3f7955f8bf3dbc8578c313b4b36bfe26bae632d1
Russell Holmes <rholmes@truenorthtechnology.com>
2018-10-09 20:39:47 -0400

    updated Gem version

--- lib/data_migrator/version.rb
+++ lib/data_migrator/version.rb
@@ -1,9 +1,9 @@
 module DataMigrator
   module VERSION #:nodoc:
     MAJOR = 3
-    MINOR = 0
-    TINY  = 0
+    MINOR = 1
+    TINY = 0
 
-    STRING = [MAJOR, MINOR, TINY].compact.join('.')
+    STRING = [MAJOR, MINOR, TINY].compact.join(".")
   end
 end
